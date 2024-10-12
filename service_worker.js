5aacec6738034ffec51c3beaad303674f5d47a0e
Eren <95621105+ErenS61@users.noreply.github.com>
2024-10-12 22:36:35 +0200

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'amicale-geii-cache-v1';  // Nom du cache avec version
+const CACHE_NAME = 'amicale-geii-cache-v2';  // Nom du cache avec version
 const urlsToCache = [
   '/',
   '/index.html',
